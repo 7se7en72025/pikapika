cdd05d8feb81622f17fc6229f976fccacca1ba59
7se7en72025 <f20241312@pilani.bits-pilani.ac.in>
2026-05-14 04:16:51 +0530

    Add lexer peek/advance and helper methods

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,11 @@
 package lexer
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
 
 type TokenKind int
 
@@ -85,4 +90,49 @@ func New(input string) *Lexer {
 		column: 1,
 		offset: 0,
 	}
+}
+
+func (l *Lexer) peek() rune {
+	if l.index >= len(l.runes) {
+		return 0
+	}
+	return l.runes[l.index]
+}
+
+func (l *Lexer) peekN(n int) rune {
+	idx := l.index + n
+	if idx < 0 || idx >= len(l.runes) {
+		return 0
+	}
+	return l.runes[idx]
+}
+
+func (l *Lexer) advance() rune {
+	if l.index >= len(l.runes) {
+		return 0
+	}
+	r := l.runes[l.index]
+	l.index++
+	l.offset += utf8.RuneLen(r)
+	if r == '\n' {
+		l.line++
+		l.column = 1
+	} else {
+		l.column++
+	}
+	return r
+}
+
+func (l *Lexer) skipWhitespace() {
+	for unicode.IsSpace(l.peek()) {
+		l.advance()
+	}
+}
+
+func (l *Lexer) readWhile(pred func(rune) bool) string {
+	var builder strings.Builder
+	for current := l.peek(); current != 0 && pred(current); current = l.peek() {
+		builder.WriteRune(l.advance())
+	}
+	return builder.String()
 }

3c52b3d1f367c42a433895cf8ac3d8b3ef4fe772
7se7en72025 <f20241312@pilani.bits-pilani.ac.in>
2026-05-15 12:56:09 +0530

    Add AST Span and Node/Stmt/Expr interfaces

--- ast/ast.go
+++ ast/ast.go
@@ -1,1 +1,21 @@
 package ast
+
+type Span struct {
+	start  int
+	end    int
+	line   int
+	column int
+}
+
+type Node interface {
+	nodeKind() string // node ka type boltaa
+	Span() Span       // gives node ka strt nd end posns
+}
+
+type Stmt interface {
+	Node
+}
+
+type Expr interface {
+	Node
+}

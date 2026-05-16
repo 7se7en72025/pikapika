ff8e676b8bbf4b4c26738bca70d18a7e77f9462a
7se7en72025 <f20241312@pilani.bits-pilani.ac.in>
2026-05-16 16:21:46 +0530

    Add AST nodes for functions, vars, and control flow

--- ast/ast.go
+++ ast/ast.go
@@ -19,3 +19,44 @@ type Stmt interface {
 type Expr interface {
 	Node
 }
+
+type Param struct {
+	Name string
+	S    Span
+}
+
+type VarDecl struct {
+	Name  string
+	Value Expr
+	S     Span
+}
+
+type BlockStmt struct {
+	Stmts []Stmt
+	S     Span
+}
+type IfStmt struct {
+	Cond Expr
+	Then *BlockStmt
+	Else *BlockStmt
+	S    Span
+}
+
+type FnDecl struct {
+	Name   string
+	Params []Param
+	Body   *BlockStmt
+	S      Span
+}
+
+func (f *FnDecl) nodeKind() string { return "FnDecl" }
+func (f *FnDecl) Span() Span       { return f.S }
+
+func (p *Param) nodeKind() string { return "Param" }
+func (p *Param) Span() Span       { return p.S }
+
+func (v *VarDecl) nodeKind() string { return "VarDecl" }
+func (v *VarDecl) Span() Span       { return v.S }
+
+func (i *IfStmt) nodeKind() string { return "IfStmt" }
+func (i *IfStmt) Span() Span       { return i.S }
